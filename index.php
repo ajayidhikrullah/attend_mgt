bfc8c77b11fdcd144c5d58ae143ed5e9a68cf240
Ajayi <ajayidhikrullah@gmail.com>
2022-09-28 11:26:56 +0100

    added form and datepicker function

--- index.php
+++ index.php
@@ -6,6 +6,45 @@
 
 ?>
 
-    <h1>Hello, world!</h1>
+        <h1 class="text-center">Registration for Conference</h1>
+        <form>
+            <div class="mb-3">
+                <label for="firstname" class="form-label">First Name</label>
+                <input type="text" class="form-control" id="firstname" placeholder="your first name">
+            </div>
+            <div class="mb-3">
+                <label for="lastname" class="form-label">Last Name</label>
+                <input type="text" class="form-control" id="lastname" placeholder="your last name">
+            </div>
+            <div class="mb-3">
+                <label for="dob" class="form-label">Date of Birth </label>
+                <input type="text" class="form-control" id="dob">
+            </div>
+            <div class="mb-3">
+                <label for="specialty" class="form-label">Area of Expertise </label>
+                <!-- <input type="text" class="form-control" id="specialty"> -->
+                <select class="form-control" id="specialty">
+                    <option value="">DB Admin</option>
+                    <option value="">Project Manager</option>
+                    <option value="">Cybersecurity</option>
+                </select>
+            </div>
+            <div class="mb-3">
+                <label for="email" class="form-label">Email address</label>
+                <input type="email" class="form-control" id="email" aria-describedby="emailHelp" placeholder="type in your email">
+                <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+            </div>
+            <div class="mb-3">
+                <label for="phone" class="form-label">Contact Number</label>
+                <input type="text" class="form-control" id="phone" aria-describedby="phoneHelp">
+                <div id="phoneHelp" class="form-text">We'll never share your number with anyone else.</div>
+            </div>
 
+            <div class="d-grid gap-2 col-6 mx-auto">
+                <button type="submit" class="btn btn-primary btn-block">Submit</button>
+
+            </div>
+
+        </form>
+    <!-- </div> -->
 <?php require_once 'includes/footer.php'; ?>
