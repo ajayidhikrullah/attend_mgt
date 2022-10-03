fa83ba92f9035cb5c0a1bc54c6ad85ef272a6133
Ajayi <ajayidhikrullah@gmail.com>
2022-10-03 09:31:05 +0100

    added try and catch error handler to function

--- db/crud.php
+++ db/crud.php
@@ -9,7 +9,6 @@
         //insert record in DB
         public function insertAttendee($fname, $lname, $dob, $spec, $email, $contact){
             try {
-                //code...
                 $sql = "INSERT INTO attendee_tb (`firstname`, `lastname`, `dob`, `specialty_id`, `email`, `phone`) VALUES (:fname,:lname,:dob,:spec,:email,:contact)";
                 $stmt = $this->db->prepare($sql);
                 //bind all placeholders to the actual values
@@ -24,7 +23,6 @@
                 $stmt->execute();
                 return true;
             } catch (PDOException $e) {
-                //throw $th;
                 echo $e->getMessage();
                 return false;
             }
@@ -36,7 +34,7 @@
                 //code...
                 $sql = "UPDATE `attendee_tb` SET `firstname`=:fname, `lastname`=:lname, `dob`=:dob, `specialty_id`=:spec, `email`=:email, `phone`=:contact WHERE `attendee_id`=:id";
 
-            $stmt = $this->db->prepare($sql);
+                $stmt = $this->db->prepare($sql);
                 //bind all placeholders to the actual values
                 $stmt->bindparam(':id',$id);
                 $stmt->bindparam(':fname',$fname);
@@ -45,12 +43,9 @@
                 $stmt->bindparam(':spec', $spec);
                 $stmt->bindparam(':email', $email);
                 $stmt->bindparam(':contact', $contact);
-                
-                // execute statement
                 $stmt->execute();
                 return true;
             } catch (PDOException $e) {
-                //throw $th;
                 echo $e->getMessage();
                 return false;
             }
@@ -71,24 +66,38 @@
         }
 
         public function getAttendee(){
-            $sql = "SELECT * FROM `attendee_tb` attd INNER JOIN specialty_tb spec ON attd.specialty_id = spec.specialty_id ORDER BY `attendee_id` DESC";
-            $result = $this->db->query($sql);
-            return $result;
+            try {
+                $sql = "SELECT * FROM `attendee_tb` attd INNER JOIN specialty_tb spec ON attd.specialty_id = spec.specialty_id ORDER BY `attendee_id` DESC";
+                $result = $this->db->query($sql);
+                return $result;
+            } catch (\PDOException $e) {
+                echo $e->getMessage();
+                return false;
+            }
         }
 
         public function getAttendeeDetail($id){
-            $sql = "SELECT * FROM attendee_tb attend INNER JOIN specialty_tb spec on attend.specialty_id = spec.specialty_id WHERE attendee_id = :id";
-            $stmt = $this->db->prepare($sql);
-            $stmt->bindparam(':id', $id);
-            $stmt->execute();
-            $result = $stmt->fetch();
-            return $result;
+            try {
+                //code...
+                $sql = "SELECT * FROM attendee_tb attend INNER JOIN specialty_tb spec on attend.specialty_id = spec.specialty_id WHERE attendee_id = :id";
+                $stmt = $this->db->prepare($sql);
+                $stmt->bindparam(':id', $id);
+                $stmt->execute();
+                $result = $stmt->fetch();
+                return $result;
+            } catch (\PDOException $e) {
+                echo $e->getMessage();
+            }
         }
 
         public function getSpecialty(){
-            $sql = "SELECT * FROM specialty_tb";
-            $result = $this->db->query($sql);
-            return $result;
+            try {
+                $sql = "SELECT * FROM specialty_tb";
+                $result = $this->db->query($sql);
+                return $result;
+            } catch (\PDOException $e) {
+                echo $e->getMessage();
+            }
         }
 
 
