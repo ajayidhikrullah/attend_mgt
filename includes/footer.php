5a967dd0ae6b861f5ccc44f461fbb529fec716cc
Ajayi Dhikrullah <ajayidhikrullah.com>
2022-11-02 21:58:29 +0000

    changed chart y-axis and x-axis to month

--- includes/footer.php
+++ includes/footer.php
@@ -30,17 +30,19 @@
         const myChart = new Chart(ctx, {
         type: 'bar',
         data: {
-            labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+            labels: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
             datasets: [{
                 label: '# of Votes',
-                data: [12, 19, 3, 5, 2, 3],
+                data: [12, 19, 3, 5, 2, 3, 6, 4, 7, 2, 1, 9],
                 backgroundColor: [
                     'rgba(255, 99, 132, 0.2)',
                     'rgba(54, 162, 235, 0.2)',
                     'rgba(255, 206, 86, 0.2)',
                     'rgba(75, 192, 192, 0.2)',
                     'rgba(153, 102, 255, 0.2)',
-                    'rgba(255, 159, 64, 0.2)'
+                    'rgba(255, 159, 64, 0.2)',
+                    'rgba(255, 206, 86, 0.2)',
+
                 ],
                 borderColor: [
                     'rgba(255, 99, 132, 1)',
@@ -48,7 +50,9 @@
                     'rgba(255, 206, 86, 1)',
                     'rgba(75, 192, 192, 1)',
                     'rgba(153, 102, 255, 1)',
-                    'rgba(255, 159, 64, 1)'
+                    'rgba(255, 159, 64, 1)',
+                    'rgba(75, 192, 192, 1)',
+
                 ],
                 borderWidth: 1
             }]
