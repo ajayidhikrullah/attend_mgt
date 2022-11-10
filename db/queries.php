4462e240d312b9894102bc4d24afeacdd83bd171
Ajayi Dhikrullah <ajayidhikrullah@gmail.com>
2022-11-10 11:26:17 +0000

    added foriegn key to events TB

--- db/queries.php
+++ db/queries.php
@@ -14,17 +14,12 @@
 
         public function runQuery(){
             try {
-                $query = "ALTER TABLE events_tb ADD `users_id` INT";
+                // $query = "ALTER TABLE events_tb ADD `users_id` INT";
+                $query = "ALTER TABLE `events_tb` ADD CONSTRAINT `fk_event_user` FOREIGN KEY (`users_id`) REFERENCES `users`(`users_id`) ON DELETE RESTRICT ON UPDATE RESTRICT";
                 // $query = "SELECT * FROM `attendee_tb` WHERE `email` = 'ajayidhikrullah@gmail.com'";
                 $stmt = $this->db->query($query);
                 // var_dump($stmt);exit;
                 $stmt->execute();
-                // return $stmt;
-                    // echo "Successfully inserted";
-                    // echo("Error description: " . mysqli_error($this->db));
-                // -- add fKey user_id to eventTB
-                // "ALTER TABLE `events_tb` ADD CONSTRAINT `fk_event_user` FOREIGN KEY (`users_id`) REFERENCES `users`(`users_id`) ON DELETE RESTRICT ON UPDATE RESTRICT";
-                //throw $th;
                 return true;
             } catch (\PDOException $e) {
                 echo $e->getMessage();
